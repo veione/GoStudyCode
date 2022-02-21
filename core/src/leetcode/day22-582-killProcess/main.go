d6b746654f3b46bc65b833a6e19a3020646e47d2
wackery <wtq102@qq.com>
2022-02-21 20:53:45 +0800

    图的遍历

---
+++ core/src/leetcode/day22-582-killProcess/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func killProcess(pid, ppid []int, kill int) []int {
+	// 建图
+	m := make(map[int][]int, len(pid))
+
+	for i := range pid {
+		if ppid[i] != 0 {
+			if _, ok := m[ppid[i]]; !ok {
+				m[ppid[i]] = make([]int, 0, 1)
+			}
+			m[ppid[i]] = append(m[ppid[i]], pid[i])
+		}
+	}
+	for key, ints := range m {
+		fmt.Print(key, ": ")
+		for i := range ints {
+			fmt.Print(ints[i], " ")
+		}
+		fmt.Println()
+	}
+	res := make([]int, 0, len(pid))
+	dfs(kill, m, res)
+	return res
+}
+
+func dfs(root int, m map[int][]int, res []int) {
+	res = append(res, root)
+	for i := range m[root] {
+		dfs(m[root][i], m, res)
+	}
+}
+
+func main() {
+	pid := []int{1, 3, 10, 5}
+	ppid := []int{3, 0, 5, 3}
+
+	res := killProcess(pid, ppid, 3)
+	for i := range res {
+		fmt.Print(res[i], " ")
+	}
+}
---
+++ core/src/leetcode/day22-Sword-offer/main.go
@@ -0,0 +1,10 @@
+package main
+
+func replaceSpace(s string) string {
+	b := []byte(s)
+	
+}
+
+func main() {
+
+}
